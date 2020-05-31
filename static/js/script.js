0d239c98112ce037ffc89942956344e69c059cf0
Asif-newaz <asif.newaz0093@gmail.com>
2020-06-01 02:49:30 +0600

    Worked with age in days project

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,24 @@
-// alert('hi');
-// console.log('Hello');
+// Challenge 1: Your age in days
 
+function ageInDays() {
+
+    var birthYear = prompt('What year were you born... Good friend?');
+
+    var ageInDaysCal = (2020 - birthYear) * 365;
+
+    var h1 = document.createElement('h1');
+
+    var textAnswer = document.createTextNode('You are' + ' ' + ageInDaysCal + ' ' + 'days old.');
+
+    h1.setAttribute('id', 'ageInDays');
+
+    h1.appendChild(textAnswer);
+
+
+    document.getElementById('flex-box-result').appendChild(h1);
+
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
